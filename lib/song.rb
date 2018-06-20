3a09a47f3612991b4fe1a9fbf64d9ad503aa0c35
Valencia Caldwell <vkcaldwell@gmail.com>
2018-06-20 20:58:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,8 +36,8 @@ class Song
     @@all.find {|song| song.name == name}
   end
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == name
-      name
+    if self.find_by_name(name) 
+      self.find_by_name
     else self.create_by_name(name) 
   
     end
