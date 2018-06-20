6786acc25fc764f8581af926ea17f9a9d5144bf4
Valencia Caldwell <vkcaldwell@gmail.com>
2018-06-20 21:54:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,8 +46,8 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    # split_filename = filename.chomp(".mp3").split(" - ")
-    split_filename = filename.chop.chop.chop.chop.split(" - ")
+    split_filename = filename.chomp(".mp3").split(" - ")
+    # split_filename = filename.chop.chop.chop.chop.split(" - ")
     song = self.new
     song.name = split_filename[(1)]
     song.artist_name = split_filename[0]
