ca8fff582ae8f2bb1c814cc8d3fccf6afd6af9b8
Valencia Caldwell <vkcaldwell@gmail.com>
2018-06-20 20:47:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,42 @@
+require pry
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  
+  def self.create
+    #instantiates a new Song
+    #saves the Song
+    #finally returns the Song
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    #instantiates a song with a name property
+    song = self.new
+    song.name = name
+    song
+  end
 
+  def self.create_by_name(string_name)
+    #instantiate 
+    #save song
+    #return the property of name 
+    song = self.new 
+    song.name = string_name
+    song.save 
+    song
+  end
+  
+  def self.find_by_name(name)
+    #find a song in @@all
+    #search by name
+    song = self
+    song.all.find {|name| song.name == name}
+  end
+  
   def self.all
     @@all
   end
